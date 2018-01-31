a1f2648c843042411a2a93aed32d3cba641eabf9
johan augustsson <j.augustsson80@gmail.com>
2018-01-31 13:20:11 +0100

    update

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ window.addEventListener('load',function(event){
   let userName = document.getElementById('userName');
   let gitHubLogIn = document.getElementById('gitHubLogIn');
   let gmailLogIn = document.getElementById("gmailLogIn");
+  let footer = document.getElementsByTagName('footer');
+
+  footer.innerhtml = "hejhej";
 
 
   gmailLogIn.addEventListener('click',function(event){
@@ -105,5 +108,22 @@ let loggOut = ()=>{
 
 let googleLoggIn = function (){
   var provider = new firebase.auth.GoogleAuthProvider();
-  console.log("hej");
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
 }
